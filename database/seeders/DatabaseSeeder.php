9263703ff147afbe861a208b4c1545629011750f
saratarekabbas <saraatarekk@icloud.com>
2022-07-11 02:36:58 +0800

    El user class is not up to date; bas 3amalt seed with my own data into the database fel databaseSeeder.php class lel categories and it is working fine. I commented out el User::factory() part lghayet ma azabat elclass

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(3)->create();
+        User::truncate(); //deletes all the existing data
+        Category::truncate();
+        Job::truncate();
+
+        //factory created
+//        User::factory(3)->create();
+
+        //specialized creation
+        Category::create([
+            'name' => 'Information Technology',
+            'slug' => 'information-technology'
+        ]);
+
+        Category::create([
+            'name' => 'Accounting',
+            'slug' => 'accounting'
+        ]);
+
+        Category::create([
+            'name' => 'Business',
+            'slug' => 'business'
+        ]);
     }
 }
