bea44fa8a53741a0076300e87bce115c3ebe238c
saratarekabbas <saraatarekk@icloud.com>
2022-07-11 04:41:02 +0800

    Created fakers and return data in CategoryFactory and JobFactory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,8 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug
         ];
     }
 }
--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,17 @@ class JobFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::Factory(), //creates any random user
+            'category_id' => Category::Factory(),
+            'title' => $this->faker->sentence,
+            'company' => $this->faker->company,
+            'slug' => $this->faker->slug,
+            'location' => $this->faker->address,
+            'salary' => $this->faker->randomElement([300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]),
+            'description' => $this->faker->sentence,
+            'requirements' => $this->faker->sentence,
+            'benefits' => $this->faker->sentence,
+            'body' => $this->faker->paragraph
         ];
     }
 }
