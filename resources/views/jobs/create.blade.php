8bd86cc44baecdf20fa94c405fb7c61ae2c883f3
saratarekabbas <saraatarekk@icloud.com>
2022-07-27 07:41:03 +0800

    table edits

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -88,4 +88,50 @@
         </form>
     </section>
 
+
+    <div class="form-container">
+        <form action="/action_page.php">
+            <div class="form-container-row">
+                <div class="form-container-col-25">
+                    <label class="form-container-label" for="fname">First Name</label>
+                </div>
+                <div class="form-container-col-75">
+                    <input class="form-container-input" type="text" id="fname" name="firstname" placeholder="Your name..">
+                </div>
+            </div>
+            <div class="form-container-row">
+                <div class="form-container-col-25">
+                    <label class="form-container-label" for="lname">Last Name</label>
+                </div>
+                <div class="form-container-col-75">
+                    <input class="form-container-input" type="text" id="lname" name="lastname" placeholder="Your last name..">
+                </div>
+            </div>
+            <div class="form-container-row">
+                <div class="form-container-col-25">
+                    <label class="form-container-label" for="country">Country</label>
+                </div>
+                <div class="form-container-col-75">
+                    <select class="form-container-select">
+                        <option value="australia">Australia</option>
+                        <option value="canada">Canada</option>
+                        <option value="usa">USA</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-container-row">
+                <div class="form-container-col-25">
+                    <label class="form-container-label" for="subject">Subject</label>
+                </div>
+                <div class="col-75">
+                    <textarea class="form-container-text" id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+                </div>
+            </div>
+            <br>
+            <div class="form-container-row">
+                <input type="submit" class="submit-form-btn " value="Submit">
+            </div>
+        </form>
+    </div>
+
 </x-layout>
