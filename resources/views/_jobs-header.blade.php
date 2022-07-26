53fa4cb02181426f6015f6422d683413dc08d43f
saratarekabbas <saraatarekk@icloud.com>
2022-07-26 18:18:07 +0800

    onto auth

--- resources/views/_jobs-header.blade.php
+++ resources/views/_jobs-header.blade.php
@@ -3,18 +3,23 @@
     <p>Explore positions top companies are hiring for!</p>
 </div>
 
-<div class="dropdown">
-    <button onclick="myFunction()" class="dropbtn">Category</button>
-    <div id="myDropdown" class="dropdown-content">
-        <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
-        @foreach($categories as $category)
-            <a href="/categories/{{$category->slug}}">{{$category->slug}}</a>
-        @endforeach
+{{--dropdown--}}
+<div class="filtering-area">
+    <div class="dropdown" class="filtering-area-col">
+        <button onclick="myFunction()" class="dropbtn">Category</button>
+        <div id="myDropdown" class="dropdown-content">
+            <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+            @foreach($categories as $category)
+                <a href="/categories/{{$category->slug}}">{{$category->slug}}</a>
+            @endforeach
+        </div>
     </div>
-</div>
 
-{{--Search--}}
-<form class="example" method="GET" action="#" style="margin:auto;max-width:300px">
-    <input type="text" placeholder="Search.." name="search" value="{{ request('search') }}">
-    <button type="submit"><i class="fa fa-search"></i></button>
-</form>
+    {{--Search--}}
+    <form class="filtering-area-col search" method="GET" action="#" style="margin:auto;max-width:300px">
+        <input type="text" placeholder="Search.." name="search" value="{{ request('search') }}">
+        <button type="submit">Search</button>
+    </form>
+
+
+</div>
