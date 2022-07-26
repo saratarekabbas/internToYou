d8fac1b7fb8afa410585cf1664c51cca4d332887
saratarekabbas <saraatarekk@icloud.com>
2022-07-26 23:17:57 +0800

    Login working

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -31,11 +31,9 @@
             <div class="login-screen__content">
                 <form class="login">
                     <div class="login__field">
-                        <i class="login__icon fas fa-user"></i>
                         <input type="text" class="login__input" placeholder="User name / Email">
                     </div>
                     <div class="login__field">
-                        <i class="login__icon fas fa-lock"></i>
                         <input type="password" class="login__input" placeholder="Password">
                     </div>
                     <button class="button login__submit">
--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -1,13 +1,12 @@
 <x-layout>
 {{--    <section>--}}
-{{--        <br><br><br><br>--}}
 {{--        <form method="POST" action="/login">--}}
 {{--            @csrf--}}
 {{--            <label for="email"> Email</label> <input type="email" name="email" id="email" required>--}}
 {{--            @error('email')--}}
 {{--            <p class="error-message">{{$message}}</p>--}}
 {{--            @enderror--}}
-{{--            <label for="password"> Password</label> <input type="pasword" name="password" id="password" required>--}}
+{{--            <label for="password"> Password</label> <input type="password" name="password" id="password" required>--}}
 {{--            @error('password')--}}
 {{--            <p class="error-message">{{$message}}</p>--}}
 {{--            @enderror--}}
@@ -20,16 +19,21 @@
     <div class="login-container">
         <div class="login-screen">
             <div class="login-screen__content">
-                <form class="login">
+                <form method="POST" action="/login" class="login">
+                    @csrf
                     <div class="login__field">
-                        <i class="login__icon fas fa-user"></i>
-                        <input type="text" class="login__input" placeholder="User name / Email">
+                        <input type="text" class="login__input" placeholder="Email" type="email" name="email" id="email" required>
+                        @error('email')
+                        <p class="error-message">{{$message}}</p>
+                        @enderror
                     </div>
                     <div class="login__field">
-                        <i class="login__icon fas fa-lock"></i>
-                        <input type="password" class="login__input" placeholder="Password">
+                        <input type="password" class="login__input" placeholder="Password" name="password" id="password" required>
+                        @error('password')
+                        <p class="error-message">{{$message}}</p>
+                        @enderror
                     </div>
-                    <button class="button login__submit">
+                    <button type="submit" class="button login__submit">
                         <span class="button__text">Log In Now</span>
                     </button>
                 </form>
